b036edec176e50f9a82f506b77c5edc8d4beea5f
melee-master <89125019+melee-master@users.noreply.github.com>
2022-06-11 19:27:12 +0530

    tweaks

--- models/task.js
+++ models/task.js
@@ -1,6 +1,14 @@
 const mongoose = require('mongoose');
 const taskSchema =  new mongoose.Schema({
-    name: String,
-    completed: Boolean,
+    name: {
+        type: String,
+        required: [true, "mandatory to specify name"],
+        trim: true,
+        maxlength: [20, "max length of characters is 20"],
+    },
+    completed: {
+        type: Boolean,
+        default: false,
+    }
 })
 module.exports = mongoose.model('Task', taskSchema);
