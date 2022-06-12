72f7a16f12bc9f5560d62a3d8f2e24a969fd8a4d
melee-master <89125019+melee-master@users.noreply.github.com>
2022-06-12 16:22:58 +0530

    7th commit

--- controllers/tasks.js
+++ controllers/tasks.js
@@ -27,23 +27,27 @@ const getTask = async (req, res) => {
         res.status(200).json({msg: err});
     }
 }
-
 const deleteTask = async (req, res)=>{
     try{
         const task  = await Task.findOneAndDelete({_id: req.params.id});
         if(!task){
-            res.status(404).json({msg:`no task found with id: ${req.params.id}`});
+            return res.status(404).json({msg:`no task found with id: ${req.params.id}`});
         }
         res.status(200).json({task});
     }catch(err){
         res.status(500).json({msg: err});
     }
-
-
 }
 
-const updateTask = (req, res) =>{
-    res.send("update a task");
+const updateTask = async (req, res) =>{
+    const task = await Task.findOneAndUpdate({_id:req.params.id}, req.body, {
+        new:true,
+        runValidators:true,
+    });
+    if(!task){
+        res.status(404).json({msg: `task with id: ${req.body.id} not found`});
+    }
+    res.status(200).json({task});
 }
 
 
