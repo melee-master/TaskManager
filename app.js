dc6b8bd8dab2f2395494d51797675c2a80434fa2
melee-master <89125019+melee-master@users.noreply.github.com>
2022-06-12 17:38:53 +0530

    Reduced Redundancy

--- app.js
+++ app.js
@@ -4,7 +4,7 @@ const express = require('express');
 const app = express();
 const tasks = require('./routes/tasks');
 
-app.use(express.static('./public'));
+
 app.use(express.json());
 
 app.use('/api/v1/tasks/', tasks);
